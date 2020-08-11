a0ef6505fcdba36f388a6df417445fa30fd7a900
Gustavo Harff <gustavo.harff@gmail.com>
2020-08-11 14:11:25 -0300

    fix: dashboard

--- packages/web/src/pages/Dashboard/index.tsx
+++ packages/web/src/pages/Dashboard/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from '../../assets/icon.png';
+
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 const Dashboard: React.FC = () => (
-  <>
+  <Container>
     <Header />
-    <Container>
-      <Link to="/notes">Go to notes</Link>
-    </Container>
-  </>
-);
+    <Content>
+      <img src={logo} alt="My Note App" />
+      <h1>Welcome to My Note App</h1>
 
+      <h3>This application does not save content in the cloud!</h3>
+
+      <Link to="/notes">Go to notes</Link>
+    </Content>
+  </Container>
+);
 export default Dashboard;

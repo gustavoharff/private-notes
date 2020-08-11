4e6d25c3044b4410c11b052f3abb21a20e7e1889
Gustavo Harff <gustavo.harff@gmail.com>
2020-08-11 14:28:52 -0300

    fix: api dashboard

--- packages/web/src/services/api.ts
+++ packages/web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-note-app.netlify.app',
+  baseURL: 'https://my-note-app-server.herokuapp.com',
 });
 
 export default api;

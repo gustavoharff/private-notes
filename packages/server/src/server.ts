72a38682e5a446e21e445d062dba624c8e214049
Gustavo <gustavo.harff@gmail.com>
2020-09-08 13:29:07 -0300

    add cors

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -10,6 +11,8 @@ import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 

c40d4a7089646eb74fcb485465d9b56abd0e15b9
Gustavo Harff <gustavo.harff@gmail.com>
2020-10-19 11:45:32 -0300

    feat: update profile

--- packages/server/src/shared/infra/http/routes/index.ts
+++ packages/server/src/shared/infra/http/routes/index.ts
@@ -3,12 +3,15 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 import notesRouter from '@modules/notes/infra/http/routes/notes.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+
+routes.use('/profile', profileRouter);
 
 routes.use('/sessions', sessionsRouter);
 
